5275d4c69525b224d8d10327fbe6f9c68fcffd1b
Taylor Barnes <taylor.a.barnes@gmail.com>
2025-07-23 15:58:19 -0400

    Initial example files

---
+++ example2/exe/src/main.cpp
@@ -0,0 +1,19 @@
+#include "mpiwrapper.h"
+#include <iostream>
+
+int main(int argc, char** argv) {
+    mp_init(&argc, &argv);
+
+    int world_size;
+    mp_comm_size(&world_size);
+
+    int world_rank;
+    mp_comm_rank(&world_rank);
+
+    if (world_rank == 0) {
+        std::cout << "World size: " << world_size << std::endl;
+    }
+
+    mp_finalize();
+    return 0;
+}
---
+++ example2/lib/src/mpiwrapper.cpp
@@ -0,0 +1,37 @@
+#include "mpiwrapper.h"
+
+#ifdef __use_mpi
+#include <mpi.h>
+#endif
+
+int mp_init( int *argc, char ***argv) {
+#ifdef __use_mpi
+  return MPI_Init(argc, argv);
+#else
+  return 0;
+#endif
+}
+
+int mp_finalize(void) {
+#ifdef __use_mpi
+  return MPI_Finalize();
+#else
+  return 0;
+#endif
+}
+
+int mp_comm_rank( int *rank ) {
+#ifdef __use_mpi
+  return MPI_Comm_rank(MPI_COMM_WORLD, rank);
+#else
+  return 0;
+#endif
+}
+
+int mp_comm_size( int *size ) {
+#ifdef __use_mpi
+  return MPI_Comm_size(MPI_COMM_WORLD, size);
+#else
+  return 0;
+#endif
+}
